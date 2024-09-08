ea8727fcd5535da51b07acaed926b1de28ea880e
raspberry pi 333 <letterbox.trung@gmail.com>
2024-09-08 15:49:27 +0700

    test ok roi

--- rp3_usb.py
+++ rp3_usb.py
@@ -7,17 +7,16 @@ ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 time.sleep(2)  # Đợi Arduino khởi động
 
 # Danh sách các số để gửi (chẵn và lẻ)
-numbers_to_send = [1, 2, 3, 4] #hehe
+numbers_to_send = [0, 0, 3, 0] #hehe
 
-for number in numbers_to_send:
-    # Gửi số đến Arduino
-    # Đóng gói 4 số nguyên thành dữ liệu nhị phân (4 số int 32-bit)
-    data = struct.pack('4i', *numbers_to_send)
-    ser.write(data.encode('utf-8'))
-    print(f"Sent: {numbers_to_send}")
-    
-    # Đợi một chút để Arduino xử lý
-    time.sleep(1)
+# Gửi số đến Arduino
+# Đóng gói 4 số nguyên thành dữ liệu nhị phân (4 số int 32-bit)
+data = struct.pack('4i', *numbers_to_send)
+ser.write(data)
+print(f"Sent: {numbers_to_send}")
+
+# Đợi một chút để Arduino xử lý
+time.sleep(1)
 
 # Đóng kết nối
 ser.close()
