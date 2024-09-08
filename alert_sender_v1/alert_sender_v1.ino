31fc54dfa8e20e037725422a704c08ee6cfc11fd
trung_pc_l <trungnd2767@gmail.com>
2024-09-09 00:30:00 +0700

    code xong 2 file code nap

--- alert_sender_v1/alert_sender_v1.ino
+++ alert_sender_v1/alert_sender_v1.ino
@@ -8,3 +8,114 @@ NỘI DUNG ĐIỂU KHIỂN
   led nguồn - báo có điện
   led vị trí - báo dựa trên cái struct nhận được
 */
+
+//khai báo chân
+#define LED_NOW 13
+#define LED_KV1 12
+#define LED_KV2 14
+#define LED_KV3 27
+#define LED_KV4 26
+
+//thư viện cho now
+#include <esp_now.h>
+#include <WiFi.h>
+
+//địa chỉ MAC của ESP32 Nhận
+uint8_t receiverMAC[] = {0xB0, 0xA7, 0x32, 0x2F, 0xA7, 0xC8};
+
+//cấu trúc dữ liệu để gửi
+typedef struct struct_message {
+  int number_1;
+  int number_2;
+  int number_3;
+  int number_4;
+} struct_message;
+
+struct_message myData;
+
+void setup() {
+  // Khởi động Serial Monitor
+  Serial.begin(9600);
+
+  // Thiết lập chân
+  pinMode(LED_NOW, OUTPUT);
+  pinMode(LED_KV1, OUTPUT);
+  pinMode(LED_KV2, OUTPUT);
+  pinMode(LED_KV3, OUTPUT);
+  pinMode(LED_KV4, OUTPUT);
+
+  // Tắt hết đèn ban đầu
+  digitalWrite(LED_NOW, LOW);
+  digitalWrite(LED_KV1, LOW);
+  digitalWrite(LED_KV2, LOW);
+  digitalWrite(LED_KV3, LOW);
+  digitalWrite(LED_KV4, LOW);
+  
+  // Khởi động Wi-Fi ở chế độ Station (trạm)
+  WiFi.mode(WIFI_STA);
+  
+  // Khởi động ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    digitalWrite(LED_NOW, HIGH);
+  }
+  
+  // Đăng ký địa chỉ MAC của ESP32 Nhận (Receiver)
+  esp_now_peer_info_t peerInfo;
+  memcpy(peerInfo.peer_addr, receiverMAC, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+}
+
+
+void loop() {
+  
+  //kiểm tra xem có dữ liệu đến từ Raspberry Pi không
+  if (Serial.available() >= 16) {
+  //đọc dữ liệu và chuyển đổi thành số nguyên
+  int so_tu_usb[4];
+  Serial.readBytes((char*)so_tu_usb, 16);
+  
+  myData.number_1 = so_tu_usb[0];
+  myData.number_2 = so_tu_usb[1];
+  myData.number_3 = so_tu_usb[2];
+  myData.number_4 = so_tu_usb[3];
+
+  esp_err_t result = esp_now_send(receiverMAC, (uint8_t *) &myData, sizeof(myData));
+  
+  //điều khiển đèn
+  if(so_tu_usb[0] > 0){
+    digitalWrite(LED_KV1, HIGH);
+  }
+  else{
+    digitalWrite(LED_KV1, LOW);
+  }
+
+  if(so_tu_usb[1] > 0){
+    digitalWrite(LED_KV2, HIGH);
+  }
+  else{
+    digitalWrite(LED_KV2, LOW);
+  }
+
+  if(so_tu_usb[2] > 0){
+    digitalWrite(LED_KV3, HIGH);
+  }
+  else{
+    digitalWrite(LED_KV3, LOW);
+  }
+  
+  if(so_tu_usb[3] > 0){
+    digitalWrite(LED_KV4, HIGH);
+  }
+  else{
+    digitalWrite(LED_KV4, LOW);
+  }
+
+  } 
+}
---
+++ rP3_code.py
@@ -0,0 +1,61 @@
+from ultralytics import YOLO
+import cv2
+
+import serial
+import time
+import struct
+
+# Cấu hình cổng serial để kết nối với ESP
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+time.sleep(2)  # Đợi ESP khởi động
+
+# Cấu hình mô hình Yolo
+model = YOLO("yolov5n_custom2.pt")
+hinh_anh_dau_vao = cv2.VideoCapture(0)
+
+def xac_dinh_vi_tri(x_center, frame_width):
+    if 0 <= x_center < frame_width / 4:
+        return 4
+    elif frame_width / 4 <= x_center < frame_width / 2:
+        return 3
+    elif frame_width / 2 <= x_center < 3 * frame_width / 4:
+        return 2
+    elif 3 * frame_width / 4 <= x_center <= frame_width:
+        return 1
+    
+def tao_mang_ket_qua(num_g, list_b):
+    list_b[num_g-1] += 1
+
+
+while True:
+
+    doc_thanh_cong, khung_hinh = hinh_anh_dau_vao.read()
+    if not doc_thanh_cong:
+        break
+
+    ket_qua = model.predict(source=khung_hinh,
+                            conf=0.3,
+                            device="cpu",
+                            classes=[0],
+                            show=False)
+
+    for vat_the in ket_qua:
+        #cái mảng này để lưu vị trí
+        list_a = [0, 0, 0, 0]
+        #cái này tính tọa độ tâm để xem ô nào có người và có bao nhiêu
+        for box in vat_the.boxes:
+            #đoạn này tính tọa độ tâm
+            x1, y1, x2, y2 = box.xyxy[0].cpu().numpy()
+            x_tam = int((x1 + x2) / 2)
+            y_tam = int((y1 + y2) / 2)
+
+            #đoạn báo xem vật thể đang ở ô nào
+            tao_mang_ket_qua(xac_dinh_vi_tri(x_tam, 640), list_a)
+
+        data = struct.pack('4i', *list_a)
+        ser.write(data)
+        print(f"Sent: {list_a}")
+
+
+
+
