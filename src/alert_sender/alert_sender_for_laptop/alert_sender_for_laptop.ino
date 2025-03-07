c1d5e7ab330ada91cd59484c2aa3086adca04e32
trung_pc_l <trungnd2767@gmail.com>
2025-03-07 22:31:18 +0700

    delête trashtrash

--- src/alert_sender/alert_sender_for_laptop/alert_sender_for_laptop.ino
+++ src/alert_sender/alert_sender_for_laptop/alert_sender_for_laptop.ino
@@ -2,76 +2,48 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define LED_PIN 4  // Chân GPIO4 để điều khiển LED
+#define LED_PIN 4
 
-// Định nghĩa địa chỉ MAC broadcast
 uint8_t receiverMAC[] = {0xE4, 0x65, 0xB8, 0x78, 0x6A, 0x50};
 
-// Định nghĩa struct chứa 8 số nguyên
 typedef struct {
     int numbers[8];
 } DataPacket;
 
 DataPacket packet;
 
-// Hàm callback khi gửi dữ liệu
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-    // Serial.print("Gửi dữ liệu: ");
-    // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Thành công" : "Thất bại");
+
 }
 
 
 void setup() {
     Serial.begin(115200);
-
-    // Đặt ESP32 vào chế độ Wi-Fi Station
     WiFi.mode(WIFI_STA);
-
-    // Khởi tạo ESP-NOW
     if (esp_now_init() != ESP_OK) {
-        // Serial.println("Lỗi khi khởi tạo ESP-NOW");
-        return;
     }
-    // Serial.println("ESP-NOW đã khởi tạo!");
 
-    // Đăng ký callback khi gửi dữ liệu
     esp_now_register_send_cb(OnDataSent);
 
-    // Cấu hình broadcast peer
     esp_now_peer_info_t peerInfo = {};
     memcpy(peerInfo.peer_addr, receiverMAC, 6);
     peerInfo.channel = 0;  
     peerInfo.encrypt = false;
 
-    // Thêm peer vào danh sách
     if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-        // Serial.println("Lỗi khi thêm peer!");
-        // return;
     }
-
-    // Serial.println("ESP32 Sender đã sẵn sàng!");
 }
 
 void loop() {
-    if (Serial.available() >= 32) {  // Mỗi int32 = 4 byte, 8 số = 32 byte
+    if (Serial.available() >= 32) {  //Mỗi int32 = 4 byte, 8 số = 32 byte
         int numbers[8];
-        Serial.readBytes((char*)numbers, 32);  // Nhận dữ liệu vào mảng
+        Serial.readBytes((char*)numbers, 32);
 
-        // Gán giá trị vào struct
         for (int i = 0; i < 8; i++) {
             packet.numbers[i] = numbers[i];
         }
 
-        // Gửi dữ liệu qua ESP-NOW broadcast
         esp_err_t result = esp_now_send(receiverMAC, (uint8_t*)&packet, sizeof(packet));
-
-        // if (result == ESP_OK) {
-        //     Serial.println("Đã gửi dữ liệu!");
-        // } else {
-        //     Serial.println("Lỗi khi gửi dữ liệu!");
-        // }
-
-        // delay(100);  // Gửi mỗi giây
 
     }
 }
--- src/desktop_app/test_main.py
+++ src/desktop_app/test_main.py
@@ -11,10 +11,10 @@ import serial
 #==========SETTING==========================================================
 model = YOLO("yolov8n.pt")  #trọng số
 hinh_anh_dau_vao = cv2.VideoCapture(0)  #camera
-ser = serial.Serial('COM10', 115200, timeout=1)  #cổng cắm bộ phát tín hiệu
+ser = serial.Serial('COM5', 115200, timeout=1)  #cổng cắm bộ phát tín hiệu
 #===========================================================================
 
-cac_doi_tuong_cam = ["bottle", "baseball bat", "cell phone", "knife"] #đối tượng cấm
+cac_doi_tuong_cam = ["bottle", "person", "cell phone", "knife"] #đối tượng cấm
 cai_dat_khu_vuc = [[] for _ in range(9)] #lưu cài đặt của 8 khu vực
 
 
@@ -47,7 +47,7 @@ class MyApp(tk.Tk):
         self.logo_label = Label(self)
         self.logo_label.pack(pady=10)
         try:
-            image = Image.open("logo_demo2.png")
+            image = Image.open("app_logo.png")
             image = image.resize((380, 120))
             self.logo = ImageTk.PhotoImage(image)
             self.logo_label.config(image=self.logo)
