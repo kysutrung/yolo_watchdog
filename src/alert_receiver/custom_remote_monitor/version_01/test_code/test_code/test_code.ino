d0478db8dbd87c5797df20a87b758f8589a5d3f7
trung_pc_l <trungnd2767@gmail.com>
2025-04-04 14:04:42 +0700

    sửa giao diện prototype 22

--- src/alert_receiver/custom_remote_monitor/version_01/test_code/test_code/test_code.ino
+++ src/alert_receiver/custom_remote_monitor/version_01/test_code/test_code/test_code.ino
@@ -38,7 +38,7 @@ bool thayDoi = 0;
 int soNhanDuoc[] = {0, 0, 0, 0, 0, 0, 0, 0};
 int soNhanDuocCuoi[] = {3, 3, 3, 3, 3, 3, 3, 3};
 
-int MENU_MODE = 1;
+int MENU_MODE = 9;
 int TRANG_THAI_COI = 2;
 int TRANG_THAI_COI_CUOI = 3;
 int BIEN_NHO_HUONG_DAN = 0;
@@ -88,19 +88,17 @@ void xemCanhBao(){
   if(millis() - lastReceiveTime > timeout){
     tft.fillScreen(TFT_BLACK);
     tft.setTextColor(TFT_RED);  // Màu chữ
-    tft.setTextSize(2);  // Kích thước chữ
     // Hiển thị số nhận được
-    tft.setCursor(12, 100);
-    tft.print("MAT KET NOI!!!!!!!");
+    tft.setCursor(40, 100);
+    tft.print("Lost Connection !");
   }
 
   if(thayDoi){
     tft.fillScreen(TFT_BLACK);
     tft.setTextColor(TFT_WHITE);  // Màu chữ
-    tft.setTextSize(2);  // Kích thước chữ
     // Hiển thị số nhận được
-    tft.setCursor(12, 25);
-    tft.print("KV1  KV2  KV3  KV4");
+    tft.setCursor(20, 25);
+    tft.print("Z1        Z2        Z3        Z4");
 
     datMauChu(soNhanDuoc[0]);
     tft.setCursor(20, 60);
@@ -116,8 +114,8 @@ void xemCanhBao(){
     tft.print(soNhanDuoc[3]);
 
     tft.setTextColor(TFT_WHITE);
-    tft.setCursor(12, 120);
-    tft.print("KV5  KV6  KV7  KV8");
+    tft.setCursor(20, 120);
+    tft.print("Z5        Z6        Z7        Z8");
     datMauChu(soNhanDuoc[4]);
     tft.setCursor(20, 155);
     tft.print(soNhanDuoc[4]);
@@ -131,14 +129,14 @@ void xemCanhBao(){
     tft.setCursor(200, 155);
     tft.print(soNhanDuoc[7]);
 
-    tft.setCursor(40, 210);
+    tft.setCursor(20, 210);
     if(TRANG_THAI_COI == 1){
       tft.setTextColor(TFT_WHITE);
-      tft.print("Am Thanh: Bat");
+      tft.print("Alarm: On");
     }
     else if(TRANG_THAI_COI == 2){
       tft.setTextColor(TFT_BLUE);
-      tft.print("Am Thanh: Tat");
+      tft.print("Alarm: Off");
     }
 
     coiCanhBao();
@@ -154,18 +152,30 @@ void xemCanhBao(){
   BIEN_NHO_HUONG_DAN = 0;
 }
 
-void huongDan(){
+void thongTin(){
   if(BIEN_NHO_HUONG_DAN == 0){
     tft.fillScreen(TFT_BLACK);
-    tft.setTextColor(TFT_WHITE);  // Màu chữ
-    tft.setTextSize(2);  // Kích thước chữ
+    tft.setTextColor(TFT_RED);  // Màu chữ
     // Hiển thị số nhận được
     tft.setCursor(20, 25);
-    tft.print("Chi Dan ------>");
-    tft.setCursor(20, 110);
-    tft.print("Thong Bao ------>");
-    tft.setCursor(20, 195);
-    tft.print("Bat/Tat Coi ---->");
+    tft.print("YOLO WATCHDOG V3.0");
+    tft.setCursor(20, 50);
+    tft.print("Remote Monitor Unit");
+    tft.setTextColor(TFT_WHITE);  // Màu chữ
+    tft.setCursor(20, 80);
+    tft.print("The function of this device");
+    tft.setCursor(20, 105);
+    tft.print("is to notify of the number of");
+    tft.setCursor(20, 130);
+    tft.print("violations in each specific");
+    tft.setCursor(20, 155);
+    tft.print("monitored area.");  
+    tft.setCursor(20, 190);
+    tft.setTextColor(TFT_RED);  // Màu chữ
+    tft.print("Designed by Trungtaulua");
+    tft.setCursor(20, 215);
+    tft.print("April 2025");
+    tft.setTextColor(TFT_WHITE);  // Màu chữ
     BIEN_NHO_HUONG_DAN = 3;
     thayDoi = 1;
   }
@@ -175,8 +185,11 @@ void hienThiManHinh(){
   if(MENU_MODE == 2){
     xemCanhBao();
   }
+  else if(MENU_MODE == 1){
+    thongTin();
+  }
   else{
-    huongDan();
+
   }
 }
 
@@ -212,6 +225,18 @@ void nhanNut() {
   trangThaiNut3Truoc = trangThaiNut3;
 }
 
+void chiDanNut(){
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextColor(TFT_WHITE);  // Màu chữ
+    // Hiển thị số nhận được
+    tft.setCursor(20, 25);
+    tft.print("Thong Tin ------>");
+    tft.setCursor(20, 110);
+    tft.print("Thong Bao ------>");
+    tft.setCursor(20, 200);
+    tft.print("Bat/Tat Coi ------>");
+}
+
 //MAIN
 void setup() {
   Serial.begin(115200);
@@ -228,8 +253,12 @@ void setup() {
   esp_now_register_recv_cb(OnDataRecv);
 
   tft.init();  // Khởi tạo màn hình
+  tft.setFreeFont(&FreeSerif9pt7b);
+  tft.setTextSize(1);  // Kích thước chữ
   tft.setRotation(0);  // Đặt hướng màn hình (0, 90, 180, 270)
   tft.fillScreen(TFT_RED);  // Màu nền đen
+
+  chiDanNut();
   
   pinMode(coiPin, OUTPUT);
   pinMode(nutMot, INPUT_PULLUP);
